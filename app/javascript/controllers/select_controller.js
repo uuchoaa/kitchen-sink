4fd1d35f3e8fa55869b50fab48b2c1ff3b96b757
Rafael Uchôa <r.da-costa-uchoa@guideline-contractors.com>
2025-11-26 18:58:01 -0300

    feat: add Select

---
+++ app/javascript/controllers/select_controller.js
@@ -0,0 +1,52 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["button", "menu", "selectedText", "hiddenInput", "option"]
+
+  connect() {
+    this.boundClickOutside = this.clickOutside.bind(this)
+  }
+
+  disconnect() {
+    document.removeEventListener("click", this.boundClickOutside)
+  }
+
+  toggle(event) {
+    event.stopPropagation()
+    
+    if (this.menuTarget.classList.contains("hidden")) {
+      this.open()
+    } else {
+      this.close()
+    }
+  }
+
+  open() {
+    this.menuTarget.classList.remove("hidden")
+    document.addEventListener("click", this.boundClickOutside)
+  }
+
+  close() {
+    this.menuTarget.classList.add("hidden")
+    document.removeEventListener("click", this.boundClickOutside)
+  }
+
+  selectOption(event) {
+    const option = event.currentTarget
+    const value = option.dataset.value
+    const label = option.dataset.label
+
+    // Atualiza valor e texto
+    this.hiddenInputTarget.value = value
+    this.selectedTextTarget.textContent = label
+
+    // Fecha o menu
+    this.close()
+  }
+
+  clickOutside(event) {
+    if (!this.element.contains(event.target)) {
+      this.close()
+    }
+  }
+}
