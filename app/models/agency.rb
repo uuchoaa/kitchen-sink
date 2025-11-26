bd259bba8b109c8de2df0e94c59e9bc45a510d66
Rafael Uchôa <r.da-costa-uchoa@guideline-contractors.com>
2025-11-26 15:24:17 -0300

    feat: add association column

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,2 +1,3 @@
 class Agency < ApplicationRecord
+  has_many :deals
 end
--- app/views/default_cruds/index.rb
+++ app/views/default_cruds/index.rb
@@ -31,6 +31,44 @@ module Views
         end
       end
 
+      def add_column(table, attr, model_class)
+        column_name = model_class.human_attribute_name(attr)
+
+        table.column(column_name) do |item|
+          if attr == "id"
+            id_link(item)
+          else
+            value = item.public_send(attr)
+
+            # Verifica se é uma associação
+            association = model_class.reflect_on_association(attr.remove("_id").to_sym)
+            if association
+              format_association(value, association)
+            else
+              format_value(value)
+            end
+          end
+        end
+      end
+
+      def format_association(value, association)
+        return nil if value.nil?
+
+        case association.macro
+        when :belongs_to, :has_one
+          # Busca o objeto relacionado e tenta exibir um atributo legível
+          related_object = association.klass.find_by(id: value)
+          return nil unless related_object
+
+          related_object.try(:name) ||
+          related_object.try(:title) ||
+          related_object.try(:email) ||
+          "#{association.klass.model_name.human} ##{related_object.id}"
+        when :has_many, :has_and_belongs_to_many
+          "#{value.count} #{association.klass.model_name.human(count: value.count)}"
+        end
+      end
+
       def format_value(value)
         case value
         when Time, DateTime, ActiveSupport::TimeWithZone
